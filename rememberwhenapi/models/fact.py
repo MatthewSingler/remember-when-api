8c6d64f17c1686d336e211571ed3b41a856014b8
Matthew Singler <matthew.singler@gmail.com>
2021-12-07 09:37:48 -0600

    all fixtures loaded

--- rememberwhenapi/models/fact.py
+++ rememberwhenapi/models/fact.py
@@ -2,4 +2,4 @@ from django.db import models
 class Fact(models.Model):
     year = models.ForeignKey("Year", on_delete=models.CASCADE)
     contents = models.CharField(max_length=500)
-    is_Approved = models.BooleanField()
+    is_approved = models.BooleanField()
