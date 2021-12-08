3e89c0c1b00643bce7f1231d5df1dfd1b5631c58
Matthew Singler <matthew.singler@gmail.com>
2021-12-08 10:05:38 -0600

    framework for fact viewset. Not completed

--- rememberwhenapi/views/fact.py
+++ rememberwhenapi/views/fact.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import action
 import datetime
-from rememberwhenapi.models import Member, Comment, FactCategory
+from rememberwhenapi.models import Member, Comment, FactCategory, Fact, fact_category
 from django.contrib.auth.models import User
 
 class FactView(ViewSet)
@@ -19,10 +19,82 @@ class FactView(ViewSet)
         Returns:
             Response -- JSON serialized fact instance
         """
-        user = User.objects.get(user=request.auth.user)
-        category = FactCategory.objects.get(pk=request.data["category"])
+        user = User.objects.get(user=request.auth.user) #should it be user or member?
+        fact_category = FactCategory.objects.get(pk=request.data["factCategoryId"])
         
         try:
-            comment = Comment.objects.create(
-                
+            fact = Fact.objects.create(
+                year = request.data['year'],
+                contents = request.data['contents'],
+                is_approved= request.data['is_approved']
             )
+            serializer = FactSerializer(fact, context={'request': request})
+            return Response(serializer.data, status=201)
+        except ValidationError as ex:
+            return Response({'reason': ex.message}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single fact
+        Returns:
+            Response -- JSON serialized fact instance
+        """
+        try:
+            fact = Fact.objects.get(pk=pk)
+            serializer = FactSerializer(fact, context={'request': request})
+            return Response(serializer.data)
+        except Fact.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+        
+    def update(self, request, pk=None):
+        """Handle PUT requests for a fact
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        user = User.objects.get(user=request.auth.user)
+        fact = Fact.objects.get(pk=pk)
+        fact.year = request.data['year'],
+        fact.contents = request.data['contents'],
+        fact.is_approved = request.data['is_approved']
+        fact_category = FactCategory.objects.get(pk=request.data["factCategoryId"])
+        fact.fact_category = fact_category
+        fact.save()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single fact
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            fact = Fact.objects.get(pk=pk)
+            fact.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Fact.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def list(self, request):
+        """Handle GET requests to facts resource
+        Returns:
+            Response -- JSON serialized list of facts
+        """
+        facts = Fact.objects.all()
+        fact_category = self.request.query_params.get('category', None)
+        if fact_category is not None:
+            facts = facts.filter(fact_category__id=fact_category)
+        serializer = FactSerializer(
+            facts, many=True, context={'request': request})
+        return Response(serializer.data)
+    
+class FactSerializer(serializers.ModelSerializer):
+    """JSON serializer for facts
+    Arguments:
+        serializer type
+    """
+    class Meta:
+        model = Fact
+        fields = ('id', 'fact_category', 'contents', 'year', 'user')
+        depth = 1
