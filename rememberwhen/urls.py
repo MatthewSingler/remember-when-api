a17bdf3cd222aef28f50a13bd5a195bbb4e49af6
Matthew Singler <matthew.singler@gmail.com>
2021-12-08 15:57:40 -0600

    list categories works

--- rememberwhen/urls.py
+++ rememberwhen/urls.py
@@ -17,10 +17,11 @@ from rest_framework import routers
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from rememberwhenapi.views import register_user, login_user, FactView
+from rememberwhenapi.views import register_user, login_user, FactView, CategoryView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'facts', FactView, 'fact')
+router.register(r'categories', CategoryView, 'category')
 
 urlpatterns = [
     path('register', register_user),
--- rememberwhenapi/views/__init__.py
+++ rememberwhenapi/views/__init__.py
@@ -1,2 +1,3 @@
 from .auth import login_user, register_user
-from .fact import FactView
+from .fact_view import FactView
+from .category_view import CategoryView
