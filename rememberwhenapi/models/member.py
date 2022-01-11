13a64a943d3ad62eaa1e6e36ade42975cc1901ec
Travis Truett <travis.truett@ambition.com>
2022-01-11 08:28:38 -0800

    Update member.py

--- rememberwhenapi/models/member.py
+++ rememberwhenapi/models/member.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # For booleans it's our Ambition best-practice to always prepend with "is_" to make it very clear it's a toggle. 
     admin = models.BooleanField()
