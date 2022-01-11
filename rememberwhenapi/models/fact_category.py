1ece2f0d5db776e8ab3e1753bb3c2739459a0d72
Travis Truett <travis.truett@ambition.com>
2022-01-11 08:27:52 -0800

    Update fact_category.py

--- rememberwhenapi/models/fact_category.py
+++ rememberwhenapi/models/fact_category.py
@@ -1,4 +1,6 @@
+# Need two lines between imports and your class, otherwise would fail Flake8 (look it up) style guide. 
 from django.db import models
 class FactCategory(models.Model):
+    # I can't figure out why this model exists... seems redundant. Why not just have Facts and Categories and let people filter accordingly?
     fact = models.ForeignKey("Fact", on_delete=models.CASCADE)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
