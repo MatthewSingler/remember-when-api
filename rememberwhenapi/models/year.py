baf86d97bd19ebddaea81b766a50679698b888fa
Travis Truett <travis.truett@ambition.com>
2022-01-11 08:31:26 -0800

    Update year.py

--- rememberwhenapi/models/year.py
+++ rememberwhenapi/models/year.py
@@ -1,4 +1,8 @@
 from django.db import models
 class Year(models.Model):
+    # I don't know what but there's got to be a more robust way to do this than just manually storing year as integer like this. 
+    # First glance I would _probably_ still use Django DateField but just store as 1986-1-1 and then filter on just year__equals=<search_string>. 
+    # https://docs.djangoproject.com/en/4.0/ref/models/fields/#datefield
+    # This will greatly future-proof for if/when you want to intoduce more granularity and you won't have to worry about a non-trivial data migration. 
     year_number = models.IntegerField()
     
