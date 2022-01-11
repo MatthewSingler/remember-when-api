558b2eb0165a2ee217b38f8516cdc3427dc740eb
Travis Truett <travis.truett@ambition.com>
2022-01-11 08:21:39 -0800

    Update category.py

--- rememberwhenapi/models/category.py
+++ rememberwhenapi/models/category.py
@@ -1,3 +1,5 @@
 from django.db import models
 class Category(models.Model):
+    # Generally speaking... any type I'm working with a Type I'm using ChoiceField. 
+    # https://docs.djangoproject.com/en/4.0/ref/models/fields/#choices
     type = models.CharField(max_length=40)
